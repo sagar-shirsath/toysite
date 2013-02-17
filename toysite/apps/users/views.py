3f7cf8e0f323e6a5ae322d4d9a81d41ec0bafcce
Sagar Shirsath <sagar.shirsath2009@gmail.com>
2013-02-17 23:12:11 +0530

    registration form

--- toysite/apps/users/views.py
+++ toysite/apps/users/views.py
@@ -12,5 +12,6 @@ def login(request):
 
 def register(request):
     form = UserForm()
+    print form;
     return render_to_response("users/register.html", {'form':form}, context_instance=RequestContext(request))
 
